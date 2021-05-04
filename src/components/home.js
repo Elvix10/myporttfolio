0c2291b6c4a3093647aa2f492e0ea93f430b6a38
Bruno Vaz <brunel55@hotmail.com>
2021-05-04 10:48:08 -0100

    need to style it

--- src/components/home.js
+++ src/components/home.js
@@ -2,18 +2,24 @@ import './styles/home.scss'
 import {Image} from 'react'
 
 
+import bg from '../ban.png'
+
+
 
 const Home=()=>{
     return(
         <section className="home">
 
             <div className="home-container">
+                <h3>Bruno</h3>
+                <h4>Vaz</h4>
+                <p>FullStack developr</p>
                
 
             </div>
 
 
-            <img src="/images/ban.png" alt="miphoto"/>
+           
             
             
         </section>
